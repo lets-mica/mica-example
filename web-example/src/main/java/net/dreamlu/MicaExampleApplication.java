9f369c54d4fd4fbcce0531a8a47fca022b3b3e83
如梦技术 <596392912@qq.com>
2019-05-15 19:54:58 +0800

    :arrow_up: mica 版本升级

--- web-example/src/main/java/net/dreamlu/MicaExampleApplication.java
+++ web-example/src/main/java/net/dreamlu/MicaExampleApplication.java
@@ -2,7 +2,12 @@ package net.dreamlu;
 
 import net.dreamlu.mica.launcher.MicaApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+
+import javax.servlet.ServletContainerInitializer;
 
 /**
  * 示例 项目
@@ -11,10 +16,16 @@ import org.springframework.cache.annotation.EnableCaching;
  */
 @EnableCaching
 @SpringBootApplication
-public class MicaExampleApplication {
+public class MicaExampleApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		MicaApplication.run("mica-example-web", MicaExampleApplication.class, args);
 		System.out.println("http://localhost:8083/swagger-ui.html");
+	}
+
+	@Override
+	protected SpringApplicationBuilder createSpringApplicationBuilder() {
+		// war 包启动方式，注意继承 SpringBootServletInitializer，实现 createSpringApplicationBuilder 方法
+		return MicaApplication.createSpringApplicationBuilder("mica-example-web", MicaExampleApplication.class);
 	}
 }
