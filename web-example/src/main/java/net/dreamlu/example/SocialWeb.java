ebcc51492d7bf7b2b22ee6b0b65a939a2cd376a2
如梦技术 <596392912@qq.com>
2019-08-06 21:51:17 +0800

    :arrow_up: 升级 mica

--- web-example/src/main/java/net/dreamlu/example/SocialWeb.java
+++ web-example/src/main/java/net/dreamlu/example/SocialWeb.java
@@ -1,40 +1,40 @@
 package net.dreamlu.example;
 
-import lombok.AllArgsConstructor;
 import net.dreamlu.mica.common.support.IController;
-import net.dreamlu.mica.social.config.AuthConfig;
+import net.dreamlu.mica.social.AuthRequestFactory;
+import net.dreamlu.mica.social.config.AuthSource;
 import net.dreamlu.mica.social.model.AuthResponse;
-import net.dreamlu.mica.social.request.AuthDingTalkRequest;
-import net.dreamlu.mica.social.request.AuthRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@AllArgsConstructor
 public class SocialWeb implements IController {
-	private final AuthDingTalkRequest authDingTalkRequest;
+	@Autowired
+	private AuthRequestFactory authRequestFactory;
 
 	@GetMapping("auth")
 	public String auth() {
-//		AuthRequest authRequest = new AuthDingTalkRequest(AuthConfig.builder()
-//			.clientId("dingoab7pkxxteufaoc50x")
-//			.clientSecret("pclr4hldAHhhlZxQmsxdLNMhrYpl5bnCFmX56zAYT8BgdqDF-dVhCORfaTZoO2hz")
-//			.redirectUri("http://frp.dreamlu.net/callback")
-//			.build());
-		// 返回授权页面，可自行调整
-		return redirect(authDingTalkRequest.authorize());
+		AuthSource[] values = AuthSource.values();
+
+		StringBuilder html = new StringBuilder();
+		for (AuthSource authSource : values) {
+			html.append("<a href=\"/auth/").append(authSource.name().toLowerCase()).append("\">")
+				.append(authSource.name()).append("</a>").append("<br>");
+		}
+		return html.toString();
 	}
 
-	@GetMapping("callback")
+	@GetMapping("auth/{source}")
+	public String auth(@PathVariable String source) {
+		return redirect(authRequestFactory.get(source).authorize());
+	}
+
+	@GetMapping("callback/{source}")
 	@ResponseBody
-	public AuthResponse callback(String code) {
-//		AuthRequest authRequest = new AuthDingTalkRequest(AuthConfig.builder()
-//			.clientId("dingoab7pkxxteufaoc50x")
-//			.clientSecret("pclr4hldAHhhlZxQmsxdLNMhrYpl5bnCFmX56zAYT8BgdqDF-dVhCORfaTZoO2hz")
-//			.redirectUri("http://www.dreamlu.net/api/dingding/callback")
-//			.build());
-		// 授权登录后会返回一个code，用这个code进行登录
-		return authDingTalkRequest.login(code);
+	public AuthResponse callback(@PathVariable String source, String code) {
+		return authRequestFactory.get(source).login(code);
 	}
 }
